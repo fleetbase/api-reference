671315de14c939d49f93207ae8e3507655b94e83
Ronald A. Richardson <me@ron.dev>
2023-05-26 14:54:23 +0800

    set root url to `/api-reference`

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
     let ENV = {
         modulePrefix: '@fleetbase/api-reference',
         environment,
-        rootURL: '',
+        rootURL: environment === 'production' ? '/api-reference' : '/',
 		locationType: 'auto',
         EmberENV: {
             FEATURES: {
