d390a21a6a0872734a2c63c57dfc15a5782591a2
Ronald A. Richardson <me@ron.dev>
2023-05-26 15:28:40 +0800

    patch baseUrl utility method

--- app/utils/base-url.js
+++ app/utils/base-url.js
@@ -1,3 +1,8 @@
-export default function baseUrl() {
-    return true;
+import { getOwner } from '@ember/application';
+
+export default function baseUrl(path = '') {
+    const config = getOwner(this).resolveRegistration('config:environment');
+    const rootURL = config.rootURL || '/'; // Use '/' as fallback if rootURL is not defined
+
+    return `${rootURL}${path}`;
 }
